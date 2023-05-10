94fb2e4f139119c8a0ec3f84841ccab5d8bddc0a
AagieKLZ <darkyelan@gmail.com>
2023-05-10 13:50:39 +0200

    Fixed size

--- app/coffee/page.tsx
+++ app/coffee/page.tsx
@@ -45,8 +45,20 @@ export default function Page({ }: Props) {
                                         Produced by the Stille family entirely of the Gesha (also spelled Geisha) variety of Arabica, and processed by the washed method (fruit skin and flesh are removed from the beans before they are dried). This is an early harvest of the Monarch Estate Hawaiian Gesha and roasted a bit darker than the farm’s main harvest of that variety. Monarch Coffee is an award-winning coffee farm and specialty roaster of 100% estate-grown Kona coffee on the Big Island of Hawai’i. Visit www.monarchcoffee.com for more information.
                                     </div>
                                     <div className='flex w-full flex-row items-center justify-between mt-4'>
-                                        <a href='#' className='py-1 w-36 bg-orange-700 hover:bg-orange-600 text-black px-2 text-center rounded-lg'>Learn More</a>
-                                        <a href='#' className='bg-emerald-700 hover:bg-emerald-600 py-1 px-2 text-center rounded-lg text-black ml-4 w-24'>Buy</a>
+                                        <a href='#' className='py-2 w-36 bg-orange-700 hover:bg-orange-600 text-black px-2 text-center rounded-lg flex justify-between'>
+                                            <div>Learn More</div>
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                                <path fillRule="evenodd" d="M3.75 12a.75.75 0 01.75-.75h13.19l-5.47-5.47a.75.75 0 011.06-1.06l6.75 6.75a.75.75 0 010 1.06l-6.75 6.75a.75.75 0 11-1.06-1.06l5.47-5.47H4.5a.75.75 0 01-.75-.75z" clipRule="evenodd" />
+                                            </svg>
+
+                                        </a>
+                                        <a href='#' className='bg-emerald-700 hover:bg-emerald-600 py-2 px-2 text-center rounded-lg text-black ml-4 w-32 flex justify-between items-center'>
+                                            <div>Buy</div>
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                                <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+                                            </svg>
+
+                                        </a>
                                     </div>
                                 </div>
                             </div>

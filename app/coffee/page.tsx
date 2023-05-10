bbdae158a34e57f7a1df3f81137d06e9924d27c8
AagieKLZ <darkyelan@gmail.com>
2023-05-10 13:48:03 +0200

    Text format

--- app/coffee/page.tsx
+++ app/coffee/page.tsx
@@ -16,15 +16,15 @@ export default function Page({ }: Props) {
                 </div>
                 <div className='grid xl:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10 my-8 pb-4 w-full place-items-center'>
                     {
-                        Array.from({ length: 8 }, (_, i) => i).map((_, i) => (
+                        Array.from({ length: 9 }, (_, i) => i).map((_, i) => (
                             <div key={i} className='w-full h-fit relative rounded-xl mx-2'>
                                 <div className='absolute h-full w-full p-1 top-0 left-0 rounded-xl z-20'>
                                     <Image src={"/coffee.jpg"} alt="coffee" width={1000} height={1000} className='rounded-xl bg-opacity-50 w-full h-full object-cover' />
                                 </div>
                                 <div className='h-1/2 relative top-1/2 bg-opacity-50 backdrop-blur-sm bg-orange-950 rounded-xl p-4 z-50'>
-                                    <div className='text-white text-xl'>Brazil Peixoto Peaberry</div>
+                                    <div className='text-white text-xl font-semibold'>Brazil Peixoto Peaberry</div>
                                     <div className='w-full flex flex-row space-x-4 flex-wrap'>
-                                        <div className='mt-2 flex flex-row justify-start items-center rounded-full w-fit px-4 py-1 bg-emerald-900 text-white'>
+                                        <div className='mt-4 flex flex-row justify-start items-center rounded-full w-fit px-4 py-1 bg-emerald-900 text-white'>
 
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill='white'>
                                                 <path d="M6 18a6.06 6.06 0 0 0 5.17-6 7.62 7.62 0 0 1 6.52-7.51l2.59-.37c-.07-.08-.13-.16-.21-.24-3.26-3.26-9.52-2.28-14 2.18C2.28 9.9 1 15 2.76 18.46z"></path>
@@ -32,7 +32,7 @@ export default function Page({ }: Props) {
                                             </svg>
                                             <div className='ml-2'>Dark</div>
                                         </div>
-                                        <div className='mt-2 flex flex-row justify-start items-center rounded-full w-fit px-4 py-1 bg-yellow-900 text-white'>
+                                        <div className='mt-4 flex flex-row justify-start items-center rounded-full w-fit px-4 py-1 bg-yellow-900 text-white'>
 
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                                 <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM8.547 4.505a8.25 8.25 0 1011.672 8.214l-.46-.46a2.252 2.252 0 01-.422-.586l-1.08-2.16a.414.414 0 00-.663-.107.827.827 0 01-.812.21l-1.273-.363a.89.89 0 00-.738 1.595l.587.39c.59.395.674 1.23.172 1.732l-.2.2c-.211.212-.33.498-.33.796v.41c0 .409-.11.809-.32 1.158l-1.315 2.191a2.11 2.11 0 01-1.81 1.025 1.055 1.055 0 01-1.055-1.055v-1.172c0-.92-.56-1.747-1.414-2.089l-.654-.261a2.25 2.25 0 01-1.384-2.46l.007-.042a2.25 2.25 0 01.29-.787l.09-.15a2.25 2.25 0 012.37-1.048l1.178.236a1.125 1.125 0 001.302-.795l.208-.73a1.125 1.125 0 00-.578-1.315l-.665-.332-.091.091a2.25 2.25 0 01-1.591.659h-.18c-.249 0-.487.1-.662.274a.931.931 0 01-1.458-1.137l1.279-2.132z" clipRule="evenodd" />
@@ -41,7 +41,7 @@ export default function Page({ }: Props) {
                                             <div className='ml-2'>Asia</div>
                                         </div>
                                     </div>
-                                    <div className='text-orange-200 italic text-extralight mt-4'>
+                                    <div className='text-orange-200 text-extralight mt-4'>
                                         Produced by the Stille family entirely of the Gesha (also spelled Geisha) variety of Arabica, and processed by the washed method (fruit skin and flesh are removed from the beans before they are dried). This is an early harvest of the Monarch Estate Hawaiian Gesha and roasted a bit darker than the farm’s main harvest of that variety. Monarch Coffee is an award-winning coffee farm and specialty roaster of 100% estate-grown Kona coffee on the Big Island of Hawai’i. Visit www.monarchcoffee.com for more information.
                                     </div>
                                     <div className='flex w-full flex-row items-center justify-between mt-4'>
